6bf15e0b02988f29298cb5a2a4d65d294e081044
Tapish Jain <tapishj@splunk.com>
2024-10-14 10:37:18 -0700

    PAPP-34815: linting error

--- cisco_firepower_connector.py
+++ cisco_firepower_connector.py
@@ -342,7 +342,7 @@ class FP_Connector(BaseConnector):
         """
         ip_and_mask = self.destination_network.split("/")
         if len(ip_and_mask) == 1 or (self.ip_version == 4 and int(ip_and_mask[1]) == 32) or \
-                (self.ip_version == 6 and int(ip_and_mask[1]) == 128): #noqa
+                (self.ip_version == 6 and int(ip_and_mask[1]) == 128):  # noqa
             self.debug_print("IP is type Host")
             self.destination_dict = {"type": "Host", "value": "{0}".format(self.destination_network)}
         elif len(ip_and_mask) == 2:
